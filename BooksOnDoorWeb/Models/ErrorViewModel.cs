2e14e66e7ca302d29d6bf8ce89c3c2adfbe55512
Hemraj Lakhara <hemrajlakhara91111@gmail.com>
2023-05-11 00:40:10 +0530

    Add project files.

---
+++ BooksOnDoorWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooksOnDoorWeb.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
