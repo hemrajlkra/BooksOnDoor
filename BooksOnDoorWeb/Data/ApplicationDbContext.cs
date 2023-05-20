5fa36e1d50a9348b473f915ee40df28590145403
Hemraj Lakhara <hemrajlakhara91111@gmail.com>
2023-05-20 15:24:23 +0530

    setup ApplicationDbContext

---
+++ BooksOnDoorWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BooksOnDoorWeb.Data
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base()
+		{
+
+		}
+	}
+}
