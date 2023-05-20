55319fe0b71a8e3b02dca71459ff35148d6b04ec
Hemraj Lakhara <hemrajlakhara91111@gmail.com>
2023-05-20 15:03:45 +0530

    Connection String added

--- BooksOnDoorWeb/Models/Category.cs
+++ BooksOnDoorWeb/Models/Category.cs
@@ -2,6 +2,7 @@
 {
 	public class Category
 	{
+		// [key] : this annotation defines particular property is primary key
         public int Id { get; set; }
 		public string Name { get; set; }
 		public int DisplayOrder { get; set; }
