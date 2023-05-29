eddca5de6f3ab2516ee429e590b7f6991dc7544c
Hemraj Lakhara <hemrajlakhara91111@gmail.com>
2023-05-29 23:42:27 +0530

    category Controller created and migrations added

---
+++ BooksOnDoorWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BooksOnDoorWeb.Data;
+using BooksOnDoorWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksOnDoorWeb.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+			_db = db;
+        }
+        public IActionResult Index()
+		{
+			List<Category> objListCategory = _db.Categories.ToList(); 
+			return View();
+		}
+	}
+}
--- BooksOnDoorWeb/Data/ApplicationDbContext.cs
+++ BooksOnDoorWeb/Data/ApplicationDbContext.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BooksOnDoorWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BooksOnDoorWeb.Data
 {
-	public class ApplicationDbContext:DbContext
+	public class ApplicationDbContext : DbContext
 	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base()
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
-
+		}
+		public DbSet<Category> Categories { get; set; }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>().HasData(
+				new Category { Id = 1, Name = "Sci-fi", DisplayOrder = 1},
+				new Category { Id = 2, Name = "Bollywood", DisplayOrder = 2 }
+				);
 		}
 	}
 }
