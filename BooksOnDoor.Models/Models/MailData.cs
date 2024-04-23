6c9c3b39d0464cc71ad645cfe47750f95b36149e
hlakhara <hemrajlakhara91111@gmail.com>
2024-04-23 23:14:17 +0530

    Implementation of Contact us Page is done, Service use MailKit and authentication google.

---
+++ BooksOnDoor.Models/Models/MailData.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksOnDoor.Models.Models
+{
+    public class MailData
+    {
+        [Required]
+        public string EmailToId { get; set; }
+        public string OrgEmailId = "honeylakhara91111@gmail.com";
+        [Required]
+        public string EmailToName { get; set; }
+        [Required]
+        public string EmailSubject { get; set; }
+        [Required]
+        public string EmailBody { get; set; }
+
+    }
+}
---
+++ BooksOnDoorWeb/Areas/Customer/Controllers/ContactController.cs
@@ -0,0 +1,41 @@
+﻿using BooksOnDoor.DataAccess.Repository;
+using BooksOnDoor.DataAccess.Repository.IRepository;
+using BooksOnDoor.Models.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BooksOnDoorWeb.Areas.Customer.Controllers
+{
+	[Area("Customer")]
+	public class ContactController : Controller
+	{
+		private readonly IMailService _mailService;
+		public ContactController(IMailService mailService)
+		{
+			_mailService = mailService;
+		}
+		
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		//[ActionName("SendMail")]
+		//public bool Index(MailData mailData) => _mailService.SendMail(mailData);
+		public IActionResult Index(MailData mailData)
+		{
+
+			if (_mailService.SendMail(mailData))
+			{
+				TempData["success"] = "Thankyou for reaching us we will get back to you!!";
+				return RedirectToAction(nameof(Index));
+			}
+			else
+			{
+				TempData["error"] = "Please wait and try after sometime!!";
+				return RedirectToAction(nameof(Index));
+			}
+		}
+	}
+}
